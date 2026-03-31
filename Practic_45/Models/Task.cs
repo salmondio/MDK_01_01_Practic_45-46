01f15c46275ca6da03149286e1bba7598cda168a
asdas <adasdawdas>
2026-03-31 13:37:51 +0500

    разработаны модели данных Сержанин

---
+++ Practic_45/Models/Task.cs
@@ -0,0 +1,33 @@
+﻿namespace Practic_45.Models
+{
+    /// <summary>
+    /// Задачи
+    /// </summary>
+    public class Task
+    {
+        /// <summary>
+        /// Код
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Название задачи
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Приоритет задачи
+        /// </summary>
+        public string Priority { get; set; }
+        /// <summary>
+        /// Дата и время задачи
+        /// </summary>
+        public DateTime DateExecute { get; set; }
+        /// <summary>
+        /// Комментарий к задаче
+        /// </summary>
+        public string Comment { get; set; }
+        /// <summary>
+        /// Выполнена ли задача
+        /// </summary>
+        public bool Done { get; set; }
+    }
+}
