2389e04543e333c03edddf827479824c8bd6f361
asdas <adasdawdas>
2026-03-31 13:33:21 +0500

    настройка проекта, подключение swagger сержанин

--- Practic_45/Program.cs
+++ Practic_45/Program.cs
@@ -1,6 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddMvc(option => option.EnableEndpointRouting = true);
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Пробная версия"
+    });
+
+    String PathFile = Path.Combine(System.AppContext.BaseDirectory, "WebAppliction2.xml");
+    option.IncludeXmlComments(PathFile);
+});
+
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Пробная версия");
+});
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
