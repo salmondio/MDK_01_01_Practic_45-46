feffd2af1a9882d58a087f2e69b1412eef482886
asdas <adasdawdas>
2026-03-31 13:42:45 +0500

    разработан контекст для загрузки данных сержаинн

---
+++ Practic_45/Contexts/TaskContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Practic_45.Contexts
+{
+    /// <summary>
+    /// Контекст лдя задачек
+    /// </summary>
+    public class TaskContext : DbContext
+    {
+        /// <summary>
+        /// Данные из базы данных
+        /// </summary>
+        public DbSet<Task> Tasks { get; set; }
+        public TaskContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        /// <summary>
+        /// переопределил метод конфигурации
+        /// </summary>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=;uid=;pwd=;database=;", new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
