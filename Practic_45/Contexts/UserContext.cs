7e23d09c60da60d12e2f565de0a64fc7759fd565
asdas <adasdawdas>
2026-04-02 15:40:17 +0500

    Создание модели и контекста пользователя. Фамилия Сержанин

---
+++ Practic_45/Contexts/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practic_45.Models;
+
+namespace Practic_45.Contexts
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } 
+        public UserContext()
+        {
+            Database.EnsureCreated();
+            Users.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=10.0.201.112;uid=ISP_23_2_8;pwd=egW19je7D1_;database=base1_ISP_23_2_8;", new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
